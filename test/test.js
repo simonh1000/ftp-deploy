cf1eeb2be71c51106e9128e5bf8e6b60c7764f79
Rick Bergfalk <rick.bergfalk@gmail.com>
2014-11-09 02:32:41 -0600

    Adding simple test stuff

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var FtpDeploy = require('../ftp-deploy.js');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+    username: "rickbergfalk",
+    password: "", // optional, prompted if none given
+    host: "localhost",
+    port: 21,
+    localRoot: __dirname + "/test-root",
+    remoteRoot: "/Users/rickbergfalk/Public",
+    exclude: ['.git', '.idea', 'tmp/*']
+};
+    
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err);
+    else console.log('finished');
+});
