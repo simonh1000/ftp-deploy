e0687489aaade053b37b2efc2c8b101b57483619
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-04-08 14:21:44 -0400

    adopt xo style

--- test/test.js
+++ test/test.js
@@ -1,27 +1,32 @@
-var FtpDeploy = require('../ftp-deploy.js');
-var ftpDeploy = new FtpDeploy();
+const path = require('path');
+const FtpDeploy = require('../ftp-deploy.js');
 
-var config = {
-    username: "rickbergfalk",
-    password: "", // optional, prompted if none given
-    host: "localhost",
-    port: 21,
-    localRoot: __dirname + "/test-root",
-    remoteRoot: "/Users/rickbergfalk/Public/",
-    exclude: ['.git', '.idea', 'tmp/*']
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+	username: 'rickbergfalk',
+	password: '', // Optional, prompted if none given
+	host: 'localhost',
+	port: 21,
+	localRoot: path.join(__dirname, 'test-root'),
+	remoteRoot: '/Users/rickbergfalk/Public/',
+	exclude: ['.git', '.idea', 'tmp/*']
 };
 
-ftpDeploy.on('uploaded', function (data) {
-    console.log('uploaded:');
-    console.log(data);
+ftpDeploy.on('uploaded', data => {
+	console.log('uploaded:');
+	console.log(data);
 });
 
-ftpDeploy.on('uploading', function (data) {
-    console.log('uploading');
-    console.log(data);
+ftpDeploy.on('uploading', data => {
+	console.log('uploading');
+	console.log(data);
 });
-    
-ftpDeploy.deploy(config, function(err) {
-    if (err) console.log(err);
-    else console.log('finished');
+
+ftpDeploy.deploy(config, err => {
+	if (err) {
+		console.log(err);
+	}	else {
+		console.log('finished');
+	}
 });
