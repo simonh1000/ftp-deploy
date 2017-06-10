19d8b7066b2a4760487d63351b30e98556ebdd01
Simon Hampton <hotbelgo@gmail.com>
2017-06-10 12:00:47 +0200

    rename test content

--- test/test.js
+++ test/test.js
@@ -4,29 +4,29 @@ const FtpDeploy = require('../ftp-deploy.js');
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-	username: 'simon',
-	password: '', // Optional, prompted if none given
-	host: 'localhost',
-	port: 21,
-	localRoot: path.join(__dirname, 'test-root'),
-	remoteRoot: path.join(__dirname, 'ftpsite'),
-	exclude: ['.git', '.idea', 'tmp/*']
+    username: 'simon',
+    password: '', // Optional, prompted if none given
+    host: 'localhost',
+    port: 21,
+    localRoot: path.join(__dirname, 'local'),
+    remoteRoot: path.join(__dirname, 'remote'),
+    exclude: ['.git', '.idea', 'tmp/*']
 };
 
 ftpDeploy.on('uploaded', data => {
-	console.log('uploaded:');
-	console.log(data);
+    console.log('uploaded:');
+    console.log(data);
 });
 
 ftpDeploy.on('uploading', data => {
-	console.log('uploading');
-	console.log(data);
+    console.log('uploading');
+    console.log(data);
 });
 
 ftpDeploy.deploy(config, err => {
-	if (err) {
-		console.log(err);
-	}	else {
-		console.log('finished');
-	}
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('finished');
+    }
 });
