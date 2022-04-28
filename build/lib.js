ce033139e25af6a912b412e8d88e779ccc17f0aa
shekharkhedekar <shekhar.khedekar@gmail.com>
2022-04-28 08:46:02 -0700

    build folder, fix npm scripts and config

--- build/lib.js
+++ build/lib.js
@@ -1,15 +1,12 @@
-const fs = require("fs");
-const path = require("path");
-const util = require("util");
-const Promise = require("bluebird");
-
-const read = require("read");
-const readP = util.promisify(read);
-
-const minimatch = require("minimatch");
-
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var util = require("util");
+var Promise = require("bluebird");
+var read = require("read");
+var readP = util.promisify(read);
+var minimatch = require("minimatch");
 // P H A S E  0
-
 function checkIncludes(config) {
     config.excludes = config.excludes || [];
     if (!config.include || !config.include.length) {
@@ -17,18 +14,18 @@ function checkIncludes(config) {
             code: "NoIncludes",
             message: "You need to specify files to upload - e.g. ['*', '**/*']"
         });
-    } else {
+    }
+    else {
         return Promise.resolve(config);
     }
 }
-
 function getPassword(config) {
     if (config.password) {
         return Promise.resolve(config);
-    } else {
-        let options = {
-            prompt:
-                "Password for " +
+    }
+    else {
+        var options = {
+            prompt: "Password for " +
                 config.user +
                 "@" +
                 config.host +
@@ -36,50 +33,48 @@ function getPassword(config) {
             default: "",
             silent: true
         };
-        return readP(options).then(res => {
-            let config2 = Object.assign(config, { password: res });
+        return readP(options).then(function (res) {
+            var config2 = Object.assign(config, { password: res });
             return config2;
         });
     }
 }
-
 // Analysing local firstory
-
 function canIncludePath(includes, excludes, filePath) {
-    let go = (acc, item) =>
-        acc || minimatch(filePath, item, { matchBase: true });
-    let canInclude = includes.reduce(go, false);
-
+    var go = function (acc, item) {
+        return acc || minimatch(filePath, item, { matchBase: true });
+    };
+    var canInclude = includes.reduce(go, false);
     // Now check whether the file should in fact be specifically excluded
     if (canInclude) {
         // if any excludes match return false
         if (excludes) {
-            let go2 = (acc, item) =>
-                acc && !minimatch(filePath, item, { matchBase: true });
+            var go2 = function (acc, item) {
+                return acc && !minimatch(filePath, item, { matchBase: true });
+            };
             canInclude = excludes.reduce(go2, true);
         }
     }
     // console.log("canIncludePath", include, filePath, res);
     return canInclude;
 }
-
 // A method for parsing the source location and storing the information into a suitably formated object
 function parseLocal(includes, excludes, localRootDir, relDir) {
     // reducer
-    let handleItem = function(acc, item) {
-        const currItem = path.join(fullDir, item);
-        const newRelDir = path.relative(localRootDir, currItem);
-
+    var handleItem = function (acc, item) {
+        var currItem = path.join(fullDir, item);
+        var newRelDir = path.relative(localRootDir, currItem);
         if (fs.lstatSync(currItem).isDirectory()) {
             // currItem is a directory. Recurse and attach to accumulator
-            let tmp = parseLocal(includes, excludes, localRootDir, newRelDir);
-            for (let key in tmp) {
+            var tmp = parseLocal(includes, excludes, localRootDir, newRelDir);
+            for (var key in tmp) {
                 if (tmp[key].length == 0) {
                     delete tmp[key];
                 }
             }
             return Object.assign(acc, tmp);
-        } else {
+        }
+        else {
             // currItem is a file
             // acc[relDir] is always created at previous iteration
             if (canIncludePath(includes, excludes, newRelDir)) {
@@ -90,58 +85,51 @@ function parseLocal(includes, excludes, localRootDir, relDir) {
         }
         return acc;
     };
-
-    const fullDir = path.join(localRootDir, relDir);
+    var fullDir = path.join(localRootDir, relDir);
     // Check if `startDir` is a valid location
     if (!fs.existsSync(fullDir)) {
         throw new Error(fullDir + " is not an existing location");
     }
-
     // Iterate through the contents of the `fullDir` of the current iteration
-    const files = fs.readdirSync(fullDir);
+    var files = fs.readdirSync(fullDir);
     // Add empty array, which may get overwritten by subsequent iterations
-    let acc = {};
+    var acc = {};
     acc[relDir] = [];
-    const res = files.reduce(handleItem, acc);
+    var res = files.reduce(handleItem, acc);
     return res;
 }
-
 function countFiles(filemap) {
-    return Object.values(filemap).reduce((acc, item) => acc.concat(item))
+    return Object.values(filemap).reduce(function (acc, item) { return acc.concat(item); })
         .length;
 }
-
 function deleteDir(ftp, dir) {
-    return ftp.list(dir).then(lst => {
-        let dirNames = lst
-            .filter(f => f.type == "d" && f.name != ".." && f.name != ".")
-            .map(f => path.posix.join(dir, f.name));
-
-        let fnames = lst
-            .filter(f => f.type != "d")
-            .map(f => path.posix.join(dir, f.name));
-
+    return ftp.list(dir).then(function (lst) {
+        var dirNames = lst
+            .filter(function (f) { return f.type == "d" && f.name != ".." && f.name != "."; })
+            .map(function (f) { return path.posix.join(dir, f.name); });
+        var fnames = lst
+            .filter(function (f) { return f.type != "d"; })
+            .map(function (f) { return path.posix.join(dir, f.name); });
         // delete sub-directories and then all files
-        return Promise.mapSeries(dirNames, dirName => {
+        return Promise.mapSeries(dirNames, function (dirName) {
             // deletes everything in sub-directory, and then itself
-            return deleteDir(ftp, dirName).then(() => ftp.rmdir(dirName));
-        }).then(() => Promise.mapSeries(fnames, fname => ftp.delete(fname)));
+            return deleteDir(ftp, dirName).then(function () { return ftp.rmdir(dirName); });
+        }).then(function () { return Promise.mapSeries(fnames, function (fname) { return ftp.delete(fname); }); });
     });
 }
-
-const mkDirExists = (ftp, dir) => {
+var mkDirExists = function (ftp, dir) {
     // Make the directory using recursive expand
-    return ftp.mkdir(dir, true).catch(err => {
+    return ftp.mkdir(dir, true).catch(function (err) {
         if (err.message.startsWith("EEXIST")) {
             return Promise.resolve();
-        } else {
+        }
+        else {
             console.log("[mkDirExists]", err.message);
             // console.log(Object.getOwnPropertyNames(err));
             return Promise.reject(err);
         }
     });
 };
-
 module.exports = {
     checkIncludes: checkIncludes,
     getPassword: getPassword,
